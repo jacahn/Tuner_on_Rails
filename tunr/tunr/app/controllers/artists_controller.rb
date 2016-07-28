1529bdfc90495a63444aed2f368d79b0ee79eb7c
Jacqui Ahn <jsahn23@gmail.com>
2016-07-28 14:13:04 -0400

    adds ability to edit an artist

--- tunr/tunr/app/controllers/artists_controller.rb
+++ tunr/tunr/app/controllers/artists_controller.rb
@@ -7,24 +7,29 @@ class ArtistsController < ApplicationController
 
   end
 
-  def show
-    @artist = Artist.find(params[:id])
-    
-  end
-
   def create
     @artist = Artist.create(params[:artist])
     redirect_to ("/artists/#{@artist.id}")
   end
 
-
+  def show
+    @artist = Artist.find(params[:id])
+  end
 
   def edit
     @artist = Artist.find(params[:id])
   end
 
+  def update
+    @artist = Artist.find(params[:id])
+    @artist.update(params[:artist])
+    redirect_to("/artists/#{@artist.id}")
+  end
+
+
   def destroy
     @artist = Artist.find(params[:id])
+  redirect_to("/artists/#{@artist.id}/edit")
   end
 
 end
