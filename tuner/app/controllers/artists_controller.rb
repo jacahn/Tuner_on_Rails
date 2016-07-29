172c5a39f61fa0a4f46eeeed3d9fb0f1a7272b5f
Jacqui Ahn <jsahn23@gmail.com>
2016-07-29 15:24:42 -0400

    DRYs up code in artist_controller.rb: 'before_action'.

--- tuner/app/controllers/artists_controller.rb
+++ tuner/app/controllers/artists_controller.rb
@@ -1,12 +1,14 @@
 # This is our database query
 
 class ArtistsController < ApplicationController
+  # this is called a callback, used to DRY up code, see below
+  before_action :set_artist, only: [:show, :destroy, :edit, :update]
   def index
     @artists=Artist.all
   end
 
   def show
-    @artist = Artist.find(params[:id])
+    # @artist = Artist.find(params[:id])
   end
 
   def new
@@ -19,17 +21,17 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist= Artist.find(params[:id])
+    # @artist= Artist.find(params[:id])
     @artist.destroy
     redirect_to artists_path
   end
 
   def edit
-    @artist=Artist.find(params[:id])
+    # @artist=Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
+    # @artist = Artist.find(params[:id])
     @artist.update(artist_params)
     redirect_to artist_path(@artist)
   end
@@ -40,5 +42,8 @@ class ArtistsController < ApplicationController
     params.require(:artist).permit(:name, :photo_url, :nationality)
   end
 
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
 
 end
